eb607501e4488536da14fd34d253c113912171e4
Aiden <aiden.egf.arnold@gmail.com>
2018-12-07 15:22:54 -0800

    updated relax challenge

--- relax_challenge/helper.py
+++ relax_challenge/helper.py
@@ -25,6 +25,27 @@ class Model():
         # Generate ROC curve values: fpr, tpr, thresholds
         fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
         
+        # generate precision-recall values
+        precision, recall, pr_thresholds = precision_recall_curve(y_test, self.fitted_model.predict(X_test))
+        # calculate average precision
+        average_precision = average_precision_score(y_test, self.fitted_model.predict(X_test))
+        
+        # figure settings
+        f, (ax1, ax2) = plt.subplots(ncols=2, figsize=(15, 5))
+
+        # plot ROC curve
+        ax1.plot([0, 1], [0, 1], 'k--')
+        ax1.plot(fpr, tpr)
+        ax1.set_xlabel('False Positive Rate')
+        ax1.set_ylabel('True Positive Rate')
+        ax1.set_title('ROC Curve')
+        
+        # plot precision-recall curve
+        ax2.step(precision, recall, where='post')
+        ax2.set_xlabel('Recall')
+        ax2.set_ylabel('Precision')
+        ax2.set_title('Precion-Recall Curve')
+        plt.show()
         
         
 
@@ -34,6 +55,11 @@ class Model():
         # display model accuracy
         print(f'Accuracy on the test data is {round(accuracy_score(y_test, self.fitted_model.predict(X_test)), 3)}.')
 
+        # display AUC score
+        print(f'The model AUC for ROC curve of the test data is {round(roc_auc_score(y_test, y_pred_prob), 3)}')
+        
+        # display average precision
+        print(f'Average precision is {round(average_precision, 3)}.')
 
         if cv:
             print(f'The best parameters are {self.fitted_model.best_params_}.')
